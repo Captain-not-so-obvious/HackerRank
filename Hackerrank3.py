4525fe20a468996734c1303997089c1b7d97e961
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2025-03-24 14:41:53 -0300

    Create Hackerrank3.py
    The provided code stub reads two integers, a and b, from STDIN.
    
    Add logic to print two lines. The first line should contain the result of integer division, a // b. The second line should contain the result of float division, a / b.
    
    No rounding or formatting is necessary.
    
    Example
    a = 3
    b = 5
    The result of the integer division 3 // 5 = 0.
    The result of the float division is 3 / 5 =0.6.
    Print:
    
    0
    0.6

---
+++ Hackerrank3.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+primeiraDivisao = int(a / b)
+print(primeiraDivisao)
+
+segundaDivisao = float(a / b)
+print(segundaDivisao)
