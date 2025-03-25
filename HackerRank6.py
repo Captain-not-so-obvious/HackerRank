7c057083bf4ab191fa7bac7aee57f162130a3e3b
Fillipe Moreira <91295530+Captain-not-so-obvious@users.noreply.github.com>
2025-03-25 17:51:14 -0300

    Create HackerRank6.py
    The included code stub will read an integer, n, from STDIN.
    
    Without using any string methods, try to print the following:
    123...n
    
    Note that "..." represents the consecutive values in between.
    
    Example
    n = 5
    Print the string 12345.

---
+++ HackerRank6.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    n = int(input())
+    valorInserido = n
+    contador = 1
+    while (contador <= valorInserido):
+        print(contador, end='')
+        contador = contador + 1 
+    
